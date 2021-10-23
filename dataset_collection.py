d9a3b08dcbefeebaad517093ff055e56e6646e9e
li779 <li779@wisc.edu>
2021-10-22 21:55:35 -0400

    init

---
+++ dataset_collection.py
@@ -0,0 +1,108 @@
+from __future__ import (
+    unicode_literals,
+    absolute_import,
+    print_function,
+    division,
+    )
+
+
+import io
+import time
+from picamera import PiCamera
+# from picamerax import PiCamera
+# import picamerax.array
+import numpy as np
+import matplotlib.pyplot as plt
+from numpy.lib.stride_tricks import as_strided
+from PIL import Image
+from fractions import Fraction
+import csv
+
+def get_camera(cam_type='hq'):
+    # v1
+    if cam_type == 'v1':
+        print("V1 camera")
+        camera = PiCamera(resolution = (2592,1944))
+    # v2
+    elif cam_type == 'v2':
+        print("v2 camera")
+        camera = PiCamera(resolution=(3280, 2464))
+    # HQ
+    else:
+        print("HQ camera")
+        camera = PiCamera(resolution=(2028, 1520), framerate=Fraction(1,30), sensor_mode=2)
+    # Set ISO to the desired value
+    # exposure has precedence over this setting
+    # 100 : lower, 400:higher, [0,1600]
+    camera.iso = 100  # to set analog_gain to 1.0
+    # Let the camera warm up for a couple of seconds
+    time.sleep(2)
+    # camera.resolution = (1024, 768)
+    # camera.digital_gain = 1.0
+    camera.exposure_mode = 'off'
+
+    g = camera.awb_gains
+    print("Starting gains", g)
+
+    camera.awb_mode = 'off'
+    camera.awb_gains = 1 #(Fraction(0,256), Fraction(0,256)) #g
+
+    camera.image_denoise = False
+
+#     camera.analog_gain = Fraction(19,16)
+#     camera.digital_gain = 1
+
+    time.sleep(2)
+    print("gains", camera.awb_gains)
+    print("brightness", camera.brightness)
+    print("analog gain", camera.analog_gain)
+    print("color effect", camera.color_effects) # default None
+    # print("contrast", camera.constrast)
+    print("crop", camera.crop)
+    print("digital gain", camera.digital_gain)
+    print("drc strength", camera.drc_strength)
+    print("exposure compensation", camera.exposure_compensation)
+    print("denoising", camera.image_denoise)
+    print("meter mode", camera.meter_mode)
+    print("saturation", camera.saturation)
+    # sharpness [-100,100] , default 0 
+    print("sharpness", camera.sharpness) 
+#     print("exposure compensation", camera.exposure_compensation)
+    return camera
+
+def cap_jpeg(exposure, name=None, returnOut=False):
+    if name is None:
+        fname = './snap%05d.png'%exposure
+    else:
+        fname = name
+    framerate = 1
+    if exposure > 5e5:
+        framerate = Fraction(1,80)
+    camera.framerate = framerate
+    camera.shutter_speed = exposure
+    camera.capture(name, 'jpeg',bayer=True)
+
+
+    if returnOut:
+        print("return output")
+        image = Image.open(name)
+        output = np.array(image)
+    else: output = None
+
+    measured_exp = camera.shutter_speed
+    print("Exposure val:%5d"%measured_exp)
+    return output, measured_exp
+
+camera = get_camera('v1')
+exposure = []
+
+for i in range(6,23):
+	exposure.append(2**i)
+
+exposure.append(int(5e6))
+exposure.append(int(6e6))
+for e in exposure:
+    print(e)
+    fn = 'DiffusiveCalibration/exp%s'%(e)
+    output, _ = cap_jpeg(e, "%s.jpeg"%fn, False)
+camera.close()
